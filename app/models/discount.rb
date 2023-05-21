71646cc7a571839b3345a218b87bb15101ec85a3
Your Name <you@example.com>
2023-05-21 15:06:22 +0700

    Add model validation and association

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,8 +2,15 @@ class Discount < ApplicationRecord
     has_many :order
     has_many :booking
 
-    validates_presence_of :discountid, :amount, :startime, :endtime, :pointstandard, message: "Vui lòng điền lại thông tin"
+    validates_presence_of :discountid, :amount, :starttime, :endtime, :pointstandard, message: "Vui lòng điền lại thông tin"
     validates_uniqueness_of :discountid, message: "Thông tin đã tồn tại"
-    
-    
+    validate :endtime_after_starttime
+    def endtime_after_starttime
+       if starttime.present? && endtime.present? && endtime < starttime
+        errors.add(:endtime, "Thời gian không hợp lệ")
+       end
+    end 
 end
+    
+    
+

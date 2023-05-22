de389509708f9862b72c455b0f58645018783acf
Your Name <you@example.com>
2023-05-23 06:36:41 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'orders#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
